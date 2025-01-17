06f7d8237cf2f9f868b728261c6f7b361c1d65db
VladimirSergeev1983 <127841259+VladimirSergeev1983@users.noreply.github.com>
2025-01-17 12:08:33 +0300

    Create test_my_app.py

---
+++ tests/test_my_app.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from ..recipes_main import app
+
+client = TestClient(app)
+
+
+def test_create_item():
+    test_request = {"recipe_name": "Test recipe",
+                    "cooking_time": 10,
+                    "ingredients": "test, test, test",
+                    "description": "some test description"}
+    response = client.post(
+        "/recipes/",
+        json=test_request,
+    )
+    assert response.status_code == 200
+    assert response.json() == test_request
+
+
+def test_read_main():
+    response = client.get('/recipes/')
+    assert response.status_code == 200
+    assert response.json() is not None
+
+
+def test_read_nonexistent_item():
+    response = client.get('/recipes/123456789')
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Recipe not found"}
+
