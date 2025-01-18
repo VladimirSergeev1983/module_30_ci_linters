bfc30ce174f02c11166f87fa2ed53aae4a57e3e1
VladimirSergeev1983 <127841259+VladimirSergeev1983@users.noreply.github.com>
2025-01-18 18:38:14 +0300

    Update models_rb.py

--- my_app/models_rb.py
+++ my_app/models_rb.py
@@ -4,11 +4,10 @@ from database_rb import Base
 
 
 class Recipe(Base):
-    __tablename__ = 'Recipes'
+    __tablename__ = "Recipes"
     id = Column(Integer, primary_key=True, index=True)
     recipe_name = Column(String, index=True)
     cooking_time = Column(Integer, index=True)
     views_number = Column(Integer, index=True)
     ingredients = Column(String, nullable=False)
     description = Column(String, nullable=True)
-
