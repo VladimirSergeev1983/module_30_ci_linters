d743f157158d376cf01e4b2157a7a879b567ed95
VladimirSergeev1983 <127841259+VladimirSergeev1983@users.noreply.github.com>
2025-01-18 18:44:40 +0300

    Update recipes_main.py

--- my_app/recipes_main.py
+++ my_app/recipes_main.py
@@ -14,21 +14,21 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    print('Starting system.')
-    if not os.path.exists('recipe_book.db'):
+    print("Starting system.")
+    if not os.path.exists("recipe_book.db"):
         async with engine.begin() as conn:
-            print('Creating DB tables.')
+            print("Creating DB tables.")
             await conn.run_sync(models_rb.Base.metadata.create_all)
 
 
 @app.on_event("shutdown")
 async def shutdown():
-    print('Closing DB sessions')
+    print("Closing DB sessions")
     await session.close()
     await engine.dispose()
 
 
-@app.post('/recipes/', response_model=schemas_rb.RecipeOut)
+@app.post("/recipes/", response_model=schemas_rb.RecipeOut)
 async def recipes(recipe: schemas_rb.RecipeIn) -> models_rb.Recipe:
     new_recipe = models_rb.Recipe(**recipe.dict())
     async with session.begin():
@@ -36,21 +36,28 @@ async def recipes(recipe: schemas_rb.RecipeIn) -> models_rb.Recipe:
     return new_recipe
 
 
-@app.get('/recipes/', response_model=List[schemas_rb.RecipeOutResponse])
+@app.get("/recipes/", response_model=List[schemas_rb.RecipeOutResponse])
 async def recipes() -> List[models_rb.Recipe]:
-    res = await session.execute(select(models_rb.Recipe).order_by(models_rb.Recipe.views_number.desc(),
-                                                                  models_rb.Recipe.cooking_time.desc()))
+    res = await session.execute(
+        select(models_rb.Recipe).order_by(
+            models_rb.Recipe.views_number.desc(), models_rb.Recipe.cooking_time.desc()
+        )
+    )
     return res.scalars().all()
 
 
-@app.get('/recipes/{idx}', response_model=schemas_rb.RecipeOutSingle)
+@app.get("/recipes/{idx}", response_model=schemas_rb.RecipeOutSingle)
 async def recipes(idx: int) -> models_rb.Recipe:
-    cur_views = await session.execute(select(models_rb.Recipe.views_number).filter_by(id=idx))
+    cur_views = await session.execute(
+        select(models_rb.Recipe.views_number).filter_by(id=idx)
+    )
     views_num = cur_views.scalar_one_or_none()
     if views_num is None:
         raise HTTPException(status_code=404, detail="Recipe not found")
     new_views = views_num + 1
-    await session.execute(update(models_rb.Recipe).filter_by(id=idx).values(views_number=new_views))
+    await session.execute(
+        update(models_rb.Recipe).filter_by(id=idx).values(views_number=new_views)
+    )
     await session.commit()
     res = await session.execute(select(models_rb.Recipe).filter_by(id=idx))
     return res.scalars().first()
