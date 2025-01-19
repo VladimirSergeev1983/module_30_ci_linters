d322c98744e72a52640ed7d680efefda1aafaa03
VladimirSergeev1983 <127841259+VladimirSergeev1983@users.noreply.github.com>
2025-01-19 17:20:50 +0300

    Update recipes_main.py

--- my_app/recipes_main.py
+++ my_app/recipes_main.py
@@ -1,11 +1,11 @@
 import os
 from typing import List
 
-import schemas_rb
-from database_rb import Base, engine
-from database_rb import session as sess
+import .schemas_rb
+from .database_rb import Base, engine
+from .database_rb import session as sess
 from fastapi import FastAPI, HTTPException
-from models_rb import Recipe
+from .models_rb import Recipe
 from sqlalchemy import update
 from sqlalchemy.future import select
 
