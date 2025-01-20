13ecee48ad2c34c33264fddd2133a32892631adf
VladimirSergeev1983 <127841259+VladimirSergeev1983@users.noreply.github.com>
2025-01-20 16:48:04 +0300

    Update recipes_main.py

--- my_app/recipes_main.py
+++ my_app/recipes_main.py
@@ -1,7 +1,6 @@
-import os
+from contextlib import asynccontextmanager
 from typing import List
 
-import schemas_rb
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import update
 from sqlalchemy.future import select
@@ -9,44 +8,41 @@ from sqlalchemy.future import select
 from .database_rb import Base, engine
 from .database_rb import session as sess
 from .models_rb import Recipe
-
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def startup():
-    print("Starting system.")
-    if not os.path.exists("recipe_book.db"):
-        async with engine.begin() as conn:
-            print("Creating DB tables.")
-            await conn.run_sync(Base.metadata.create_all)
+from .schemas_rb import RecipeIn, RecipeOut, RecipeOutResponse, RecipeOutSingle
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    print("Closing DB sessions")
-    await sess.close()
-    await engine.dispose()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("База готова")
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    print("База очищена")
 
 
-@app.post("/recipes/", response_model=schemas_rb.RecipeOut)
-async def create_recipes(recipe: schemas_rb.RecipeIn) -> Recipe:
-    new_recipe = Recipe(**recipe.dict())
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/recipes/", response_model=RecipeOut)
+async def create_recipes(recipe: RecipeIn) -> Recipe:
+    new_recipe = Recipe(**recipe.model_dump())
     async with sess.begin():
         sess.add(new_recipe)
     return new_recipe
 
 
-@app.get("/recipes/", response_model=List[schemas_rb.RecipeOutResponse])
+@app.get("/recipes/", response_model=List[RecipeOutResponse])
 async def get_recipes() -> List[Recipe]:
     r = await sess.execute(select(Recipe).order_by(Recipe.views_number.desc()))
     return r.scalars().all()
 
 
-@app.get("/recipes/{idx}", response_model=schemas_rb.RecipeOutSingle)
+@app.get("/recipes/{idx}", response_model=RecipeOutSingle)
 async def get_recipes_by_id(idx: int) -> Recipe:
-    cur_views = await sess.execute(select(Recipe.views_number).where(id=idx))
-    views_num = cur_views.scalar_one_or_none()
+    my_view = await sess.execute(select(Recipe.views_number).filter_by(id=idx))
+    views_num = my_view.scalar_one_or_none()
     if views_num is None:
         raise HTTPException(status_code=404, detail="Recipe not found")
     new_views = views_num + 1
